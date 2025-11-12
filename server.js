79abf50d2fb48e4ca77f11fa88c7c44abd4030ee
ghostframe <marcosandresdiaz@gmail.com>
2025-11-12 10:30:50 -0300

    remove interactive example

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const port = 9090;
 
 const app = express();
 
-// this is the signing key that Metabase provides in settings->admin->auth->JWT, which is used to sign the JWT token for interactive embedding
-const JWT_SIGNING_KEY_INTERACTIVE_EMBEDDING =
+// this is the signing key that Metabase provides in settings->admin->auth->JWT, which is used to sign the JWT token
+const JWT_SIGNING_KEY =
   "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
 
 app.use(express.static(path.join(__dirname, "static")));
@@ -21,23 +21,7 @@ app.get("/api/auth", (req, res) => {
     groups: ["viewer"], // groups property is optional, we're sending this to show how you can configure group mappings in Metabase
   };
 
-  const isSdkRequest = req.query.response === "json";
-
-  if (isSdkRequest) {
-    console.log("SDK request detected, returning JWT in JSON response");
-    res.json({ jwt: jwt.sign(user, JWT_SIGNING_KEY_INTERACTIVE_EMBEDDING) });
-    return;
-  } else {
-    console.log("Non-SDK request detected, redirecting to Metabase SSO");
-    const METABASE_URL = "http://localhost:3000";
-    res.redirect(
-      `${METABASE_URL}/auth/sso?jwt=${jwt.sign(
-        user,
-        JWT_SIGNING_KEY_INTERACTIVE_EMBEDDING
-      )}`
-    );
-    return;
-  }
+  res.json({ jwt: jwt.sign(user, JWT_SIGNING_KEY) });
 });
 
 app.listen(port, () => {
